ce8e248a6cbdc4c6cc036844103e7b8026d91798
Alex Egg <eggie5@gmail.com>
2014-02-14 20:51:24 -0800

    implemented MSE

--- project1/subsample.m
+++ project1/subsample.m
@@ -40,11 +40,9 @@ title('orig');
 cb_20_lin = cb;
 %pixel n = (n-1 + n+1)/2
 cb_20_lin(2:2:end-1, 2:2:end-1) = (cb_20_lin(1:2:end-3, 1:2:end-3) + cb_20_lin(3:2:end-1, 3:2:end-1))/2;
-size(cb_20_lin)
 
 cr_20_lin=cr;
 cr_20_lin(2:2:end-1, 2:2:end-1) = (cr_20_lin(1:2:end-3, 1:2:end-3) + cr_20_lin(3:2:end-1, 3:2:end-1))/2;
-size(cr_20_lin)
 upscaled_lin=cat(3, y, cb_20_lin, cr_20_lin);
 
 
@@ -74,6 +72,42 @@ upscaled=cat(3, y, cb_20_rep, cr_20_rep);
 subplot(2,2,3);
 imshow(ycbcr2rgb(upscaled));
 title('pixel replcement');
+
+
+
+% 10. Measure MSE between the original and reconstructed images 
+% (obtained using linear interpolation only). Comment on the results. (10 points)
+% mse= 1/(n*m) sum(sum( f(j,k) - f2(j,k) ))
+
+y1= ycbcr(:,:,1);
+cb1=ycbcr(:,:,2);
+cr1=ycbcr(:,:,3);
+
+y2=upscaled_lin(:,:,1);
+cb2=upscaled_lin(:,:,2);
+cr2=upscaled_lin(:,:,3);
+
+dy=y1-y2;
+dcb=cb1-cb2;
+dcr=cr1-cr2;
+
+msey =  mean(dy(:).^2)
+msecb = mean(dcb(:).^2)
+msecr = mean(dcr(:).^2)
+
+%convert uint8s to doubles for maths
+MSE = mean(mean((double(ycbcr) - double(upscaled_lin)).^2,2),1);
+reshape(MSE, [1,3])
+
+
+% 11. Comment on the compression ratio achieved by subsampling Cb and Cr
+% components for 4:2:0 approach. Please note that you do not send the pixels
+% which are made zero in the row and columns during subsampling. (5 points)
+
+
+
+
+
 
 
 
