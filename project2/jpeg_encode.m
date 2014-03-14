23953540bf982c06b3804a6cec4dfcede650a903
Alex Egg <eggie5@gmail.com>
2014-03-14 14:34:41 -0700

    finished decoding - project done

--- project2/jpeg_encode.m
+++ project2/jpeg_encode.m
@@ -1,3 +1,13 @@
+% CompE 565 Project 2
+% 3/14/2014
+% Name: Alex Egg
+% Id: 809236396
+% Email: eggie5@gmail.com
+
+clear 
+clc
+
+
 RGB=imread('SunView.jpg');
 YCBCR=rgb2ycbcr(RGB);
 Y=YCBCR(:,:,1);
@@ -6,10 +16,10 @@ CR=YCBCR(:,:,3);
 
 
 %subsample cb & cr to 4:2:0
-% CB(:,2:2:end) = [];
-% CB(2:2:end,:) = [];
-% CR(:,2:2:end) = [];
-% CR(2:2:end,:) = [];
+CB(:,2:2:end) = [];
+CB(2:2:end,:) = [];
+CR(:,2:2:end) = [];
+CR(2:2:end,:) = [];
 
 %Because the DCT is designed to work on pixel values ranging from -128 to 127, the original
 %block is ?leveled off?
@@ -20,8 +30,8 @@ CR=YCBCR(:,:,3);
 dct_proc=@(block_struct) dct2(block_struct.data);
 
 ydct=blockproc(Y, [8 8], dct_proc);
-cbdct=blockproc(CB, [8 8], dct_proc);
-crdct=blockproc(CR, [8 8], dct_proc);
+cbdct=blockproc(CB, [8 8], dct_proc, 'PadPartialBlocks' ,true);
+crdct=blockproc(CR, [8 8], dct_proc, 'PadPartialBlocks', true);
 
 
 %normalize values to 0-255 range
@@ -39,21 +49,22 @@ block2=ydct(33:40, 9:16) %second block in row 4
 
 
 
-% figure()
-% block1 = imresize(block1, 50, 'box');
-% iptsetpref('ImshowAxesVisible','on');
-% imshow(block1, 'XData', [1 8], 'YData', [1 8]);
-% title('block 1')
-% xlabel('x axis') % x-axis label
-% ylabel('y axis') % y-axis label
-% 
-% figure()
-% block2 = imresize(block2, 50, 'box');
-% iptsetpref('ImshowAxesVisible','on');
-% imshow(block2, 'XData', [1 8], 'YData', [1 8]);
-% title('block 2')
-% xlabel('x axis') % x-axis label
-% ylabel('y axis') % y-axis label
+figure(1)
+subplot(1,2,1);
+block1 = imresize(block1, 50, 'box');
+iptsetpref('ImshowAxesVisible','on');
+imshow(block1, 'XData', [1 8], 'YData', [1 8]);
+title('block 1')
+xlabel('x axis') % x-axis label
+ylabel('y axis') % y-axis label
+
+subplot(1,2,2);
+block2 = imresize(block2, 50, 'box');
+iptsetpref('ImshowAxesVisible','on');
+imshow(block2, 'XData', [1 8], 'YData', [1 8]);
+title('block 2')
+xlabel('x axis') % x-axis label
+ylabel('y axis') % y-axis label
 
 
 limits = [mmin mmax];
@@ -105,10 +116,10 @@ qblock2=qy(33:40, 9:16); %second block in row 4
 qb1zig=zigzag(qblock1);
 qb2zig=zigzag(qblock2);
 
-fprintf('block 1 DC DCT Coeff: %f\n', qblock1(1));
+fprintf('block 1 DC DCT Coeff: %f\n', block1(1));
 fprintf('block 1 AC DCT coeff:\n')
 display(qb1zig);
-fprintf('block 2 DC DCT Coeff: %f\n', qblock1(1));
+fprintf('block 2 DC DCT Coeff: %f\n', block2(1));
 fprintf('block 2 AC DCT coeff:\n');
 display(qb2zig);
 
@@ -130,13 +141,42 @@ ychannel=blockproc(ydq, [8 8], idctproc);
 cbchannel=blockproc(cbdq, [8 8], idctproc);
 crchannel=blockproc(crdq, [8 8], idctproc);
 
+ychannel=uint8(ychannel);
+%cbchannel=uint8(cbchannel);
+%crchannel=uint8(crchannel);
+
+% Up-sample chroma
+upsample_filter_1d=[1 3 3 1]/4;
+upsample_filter = upsample_filter_1d' * upsample_filter_1d ; 
+cb = conv2( upsample_filter, upsample(upsample(padarray(cbchannel, [1 1], 'replicate'), 2)', 2)'); 
+cb = cb(4 : size(cb, 1) - 4, 4 : size(cb, 2) - 4);
+cr = conv2( upsample_filter,upsample(upsample(padarray(crchannel, [1 1], 'replicate'), 2)', 2)'); 
+cr = cr(4 : size(cr, 1) - 4, 4 : size(cr, 2) - 4);
+%cut off padding
+cb=cb(1:end-8,:);
+cr=cr(1:end-8,:);
 
 
 % Concatenate the three components
-YCbCr_linear = cat(3, ychannel, cbchannel, uint8(crchannel));
+YCbCr_linear = cat(3, ychannel, cb, cr);
 
 % Convert to RGB space
 RGB_reconstructed = ycbcr2rgb(YCbCr_linear);
-figure(4);
-imshow(RGB_reconstructed);
+figure(2);
+imshow(RGB);
+title('Original')
 
+figure(3);
+imshow(RGB_reconstructed);
+title('reconstructed')
+
+
+% Show the error image for the Y-component
+errorImage = abs(Y - ychannel);
+figure(4);
+imshow(errorImage, [0 max(errorImage(:))]);
+
+
+% Compute the PSNR
+MSE_Y = mean(errorImage(:).^2)
+PSNR_Y = 10 * log10(255^2/MSE_Y)
